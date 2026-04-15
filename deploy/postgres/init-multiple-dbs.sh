a8673fab5102c0cefaa97633858647d6c5d8ef42
Jhon Fredy Alzate <jfalzaed@eafit.edu.co>
2026-04-14 22:24:35 -0500

    Corrección de la base de datos al ejecutar el script de inicialización

--- deploy/postgres/init-multiple-dbs.sh
+++ deploy/postgres/init-multiple-dbs.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 create_db() {
   local db="$1"
   echo "Creating database: $db"
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
     SELECT 'CREATE DATABASE "$db"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\gexec
     GRANT ALL PRIVILEGES ON DATABASE "$db" TO "$POSTGRES_USER";
 EOSQL
